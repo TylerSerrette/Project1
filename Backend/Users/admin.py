5ef7c9db4553b9b7c8e3191d06a6b53f77df74a4
Tyler <Tylerserrette17@gmail.com>
2024-04-09 20:38:55 -0500

    wortking BE

--- Backend/Users/admin.py
+++ Backend/Users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from Users.models import UserProfile
 
-admin.site.register(UserProfile)
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(UserProfile)
--- Backend/Users/models.py
+++ Backend/Users/models.py
@@ -1,25 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_dm = models.BooleanField(default=False, verbose_name='Is Dungeon Master')
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
-
-    # Additional fields specific to D&D
-    favorite_campaign = models.CharField(max_length=255, null=True, blank=True)
-
-    # Automatically create or update a user profile when a user is created or saved
-    @receiver(post_save, sender=User)
-    def create_or_update_user_profile(sender, instance, created, **kwargs):
-        if created:
-            UserProfile.objects.create(user=instance)
-        else:
-            instance.userprofile.save()
-
+    campaigns = models.ManyToManyField('Campaign', blank=True)
+    characters = models.ManyToManyField('Character', blank=True)
+    
+    
+    def __str__(self):
+        return self.user.username
+    def __str__(self):
+        return self.user.username
     
 
-# Create your models here.
